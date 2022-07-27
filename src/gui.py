0686018651e5fbe1515edf4edae19b30ca0780b5
elxy <elxy@outlook.com>
2022-07-27 16:16:42 +0800

    add attribute display in graphic mode

--- src/gui.py
+++ src/gui.py
@@ -5,8 +5,8 @@ from typing import Dict, Callable, Optional, Union
 
 from PySide6 import QtCore, QtGui, QtWidgets
 
-from datasource import DataBuffer, FileSource
-from isobmff.box import Box
+from datasource import DataBuffer, FileSource, Position
+from isobmff.box import Box, Field
 
 
 def get_boxes_from_file(path: str, debug: bool = False) -> list[Box]:
@@ -21,8 +21,12 @@ def get_boxes_from_file(path: str, debug: bool = False) -> list[Box]:
 
 
 class Events(enum.Enum):
+    # User refreshes the view
+    REFRESH = enum.auto()
     # User selects item from box tree
     BOX_SELECTED = enum.auto()
+    # User selects attribute from attributes table
+    ATTR_SELECTED = enum.auto()
 
 
 class HTMLDelegate(QtWidgets.QStyledItemDelegate):
@@ -41,7 +45,6 @@ class HTMLDelegate(QtWidgets.QStyledItemDelegate):
         options = QtWidgets.QStyleOptionViewItem(option)
         self.initStyleOption(options, index)
         painter.save()
-        self.doc.setTextWidth(options.rect.width())
         self.doc.setHtml(options.text)
         self.doc.setDefaultFont(options.font)
         options.text = ''
@@ -61,34 +64,70 @@ class HTMLDelegate(QtWidgets.QStyledItemDelegate):
     ) -> QtCore.QSize:
         self.initStyleOption(option, index)
         self.doc.setHtml(option.text)
-        self.doc.setTextWidth(option.rect.width())
         return QtCore.QSize(self.doc.idealWidth(), self.doc.size().height())
 
 
-class BoxItem(QtWidgets.QTreeWidgetItem):
+class AttrItem(QtWidgets.QTableWidgetItem):
 
-    def set_box(self, box: Box) -> None:
-        self.box = box
+    def __init__(self, field: Field, is_name: bool) -> None:
+        self.field = field
+        self.is_name = is_name
 
-    def get_box(self) -> Box:
-        return self.box
+        string = field.name if is_name else field.get_display_value()
+        super().__init__(string)
+        self.setToolTip(string)
+        self.setFlags(self.flags() ^ QtGui.Qt.ItemIsEditable)
 
 
-class TreeView(QtWidgets.QTreeWidget):
+class AttrView(QtWidgets.QTableWidget):
 
-    def __init__(self, callbacks: Dict[Events, Callable[..., None]]) -> None:
-        super().__init__()
+    def __init__(self, callbacks: Dict[Events, Callable[..., None]], **kargs) -> None:
+        super().__init__(**kargs)
 
         self.callbacks = callbacks
 
-        self.setColumnCount(1)
-        self.setHeaderHidden(True)
+        self.setColumnCount(2)
+        self.setRowCount(0)
+        self.setSelectionBehavior(QtWidgets.QAbstractItemView.SelectRows)
+        self.setHorizontalHeaderLabels(("Field", "Value"))
+        self.verticalHeader().hide()
 
-        self.itemSelectionChanged.connect(self.on_box_selected)
+        self.itemSelectionChanged.connect(self.on_attr_selected)
 
-    def on_box_selected(self) -> None:
-        selected = self.selectedItems()[0]
-        self.callbacks[Events.BOX_SELECTED](selected.get_box())
+        self.resized = False
+
+    def on_attr_selected(self) -> None:
+        selected = self.selectedItems()
+        self.callbacks[Events.ATTR_SELECTED](selected)
+
+    def refresh(self) -> None:
+        header = self.horizontalHeader()
+        header.resizeSections(QtWidgets.QHeaderView.Stretch)
+        header.setSectionResizeMode(1, QtWidgets.QHeaderView.Stretch)
+
+    def show_attrs(self, box: Box, truncate=False) -> None:
+        for field in box.generate_fields():
+            if isinstance(field, Box):
+                continue
+            elif not isinstance(field, Field):
+                raise Exception("Expected a Field, got a %s" % type(field))
+            name_item = AttrItem(field, is_name=True)
+            value_item = AttrItem(field, is_name=False)
+            row = self.rowCount()
+            self.insertRow(row)
+            self.setItem(row, 0, name_item)
+            self.setItem(row, 1, value_item)
+
+
+class BoxItem(QtWidgets.QTreeWidgetItem, Position):
+
+    def __init__(self, box: Box) -> None:
+        QtWidgets.QTreeWidgetItem.__init__(self, [self.format_box_title(box)])
+        Position.__init__(self)
+
+        self.box = box
+        self.pos = box.buffer_offset
+        self.len = box.size
 
     def format_box_title(self, box: Box) -> str:
         root = ET.Element('span')
@@ -98,25 +137,46 @@ class TreeView(QtWidgets.QTreeWidget):
         child.text = ": %s" % (Box.getboxdesc(box.boxtype))
         return ET.tostring(root, encoding="unicode")
 
+
+class TreeView(QtWidgets.QTreeWidget):
+
+    def __init__(self, callbacks: Dict[Events, Callable[..., None]], **kargs) -> None:
+        super().__init__(**kargs)
+
+        self.callbacks = callbacks
+
+        self.setColumnCount(1)
+        self.setHeaderHidden(True)
+        delegate = HTMLDelegate()
+        self.setItemDelegate(delegate)
+        self.setMinimumWidth(320)
+        self.setMaximumWidth(480)
+
+        self.itemSelectionChanged.connect(self.on_box_selected)
+
+    def on_box_selected(self) -> None:
+        selected = self.selectedItems()[0]
+        self.callbacks[Events.BOX_SELECTED](selected)
+
     def populate(self, box: Box, parent: Optional[BoxItem] = None) -> BoxItem:
-        box_title = self.format_box_title(box)
-        item = BoxItem([box_title])
-        item.set_box(box)
+        item = BoxItem(box)
         for sub_box in box.children:
             self.populate(sub_box, item)
         if parent:
             parent.addChild(item)
         return item
 
-    def render(self, boxes: list[Box]) -> None:
+    def show_boxes(self, boxes: list[Box]) -> None:
         items = []
         for box in boxes:
             item = self.populate(box)
             items.append(item)
 
         self.insertTopLevelItems(0, items)
-        delegate = HTMLDelegate()
-        self.setItemDelegate(delegate)
+        self.setCurrentItem(items[0])
+
+        self.expandAll()
+        self.resizeColumnToContents(0)
 
 
 class View(QtWidgets.QMainWindow):
@@ -135,17 +195,28 @@ QWidget {
         self._debug = debug
 
         callbacks = {
+            Events.REFRESH: self.cb_refresh,
             Events.BOX_SELECTED: self.cb_box_selected,
+            Events.ATTR_SELECTED: self.cb_attr_selected,
         }
 
         self._tree_view = TreeView(callbacks=callbacks)
-        self.setCentralWidget(self._tree_view)
+        self._attr_view = AttrView(callbacks=callbacks)
+        self._splitter = QtWidgets.QSplitter(QtCore.Qt.Orientation.Horizontal, self)
+        self._splitter.addWidget(self._tree_view)
+        self._splitter.addWidget(self._attr_view)
+        self._splitter.setChildrenCollapsible(False)
+        self.setCentralWidget(self._splitter)
 
         self.create_actions()
 
         self.read_settings()
 
         self.setStyleSheet(View.STYLE_SHEET)
+
+    def showEvent(self, event: QtGui.QShowEvent) -> None:
+        QtWidgets.QMainWindow.showEvent(self, event)
+        self.cb_refresh()
 
     def closeEvent(self, event: QtGui.QCloseEvent) -> None:
         self.write_settings()
@@ -156,21 +227,14 @@ QWidget {
         if file_name:
             self.open_file(file_name)
 
-    def set_font(self) -> None:
-        ok, font = QtWidgets.QFontDialog.getFont(self._tree_view)
-        if ok:
-            self._tree_view.setFont(font)
-
     def create_actions(self) -> None:
         self._open_act = QtGui.QAction('&Open', self, shortcut=QtGui.QKeySequence.Open, triggered=self.open)
-        self._font_act = QtGui.QAction('&Font', self, triggered=self.set_font)
         self._exit_act = QtGui.QAction('&Quit', self, shortcut=QtGui.QKeySequence.Quit, triggered=self.close)
         self._exit_act.setVisible(False)
 
         menu_bar = self.menuBar()
 
         menu_bar.addAction(self._open_act)
-        menu_bar.addAction(self._font_act)
         menu_bar.addAction(self._exit_act)
 
     def read_settings(self) -> None:
@@ -186,11 +250,26 @@ QWidget {
     def open_file(self, file_name: str) -> None:
         boxes = get_boxes_from_file(file_name, debug=self._debug)
         self._tree_view.clear()
-        self._tree_view.render(boxes)
+        self._tree_view.show_boxes(boxes)
         self._cur_file = file_name
         self.setWindowTitle(self._cur_file)
 
-    def cb_box_selected(self, box: Box) -> None:
-        message = 'Position: {pos:d} (0x{pos:x}) | Length: {len:d} (0x{len:x}) bytes'.format(pos=box.buffer_offset,
-                                                                                             len=box.size)
+    def cb_refresh(self) -> None:
+        self._attr_view.refresh()
+
+    def cb_box_selected(self, item: BoxItem) -> None:
+        self._attr_view.setRowCount(0)
+        self._attr_view.show_attrs(item.box)
+
+        pos, len = item.get_position()
+        message = f'Position: {pos:d} (0x{pos:x}) | Length: {len:d} (0x{len:x}) bytes'
+        self.statusBar().showMessage(message)
+
+    def cb_attr_selected(self, items: list[AttrItem]) -> None:
+        if not items:
+            return
+
+        item = items[0]
+        pos, len = item.field.get_position()
+        message = f'Position: {pos:d} (0x{pos:x}) | Length: {len:d} (0x{len:x}) bytes'
         self.statusBar().showMessage(message)
